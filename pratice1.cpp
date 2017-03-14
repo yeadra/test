93cfa1de666eb8333fe20dfb74a37489e7025893
yeadra <hspss90272@gmail.com>
2017-03-14 23:05:20 +0800

    initial commit

---
+++ pratice1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(void){
+	string m("There is no spoon.");
+	cout << endl;
+	cout << m << endl;
+
+	return 0;
+}
+
+// first works	
